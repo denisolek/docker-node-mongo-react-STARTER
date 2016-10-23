2e5a9edce5fbc35dd30aa37492a5ea0f692860e6
deniso95 <denis.olek@gmail.com>
2016-10-23 20:19:07 +0200

    ItemSchema remake && added ned http methods

--- models/Item.model.js
+++ models/Item.model.js
@@ -2,9 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ItemSchema = new Schema({
-    name: String,
-    category: String,
-    count: Number
-});
+    name: {type: String, required: true},
+    category: {type: String, required: true},
+    count: {type: Number, default: 1}
+    },
+    {versionKey: false}
+);
 
 module.exports = mongoose.model('Item', ItemSchema);
