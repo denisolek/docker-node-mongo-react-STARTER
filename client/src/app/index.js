ab74588ed18a4cdcb2909ff6732d354ef6ab1e50
deniso95 <denis.olek@gmail.com>
2016-10-23 22:40:24 +0200

    testing component

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,1 +1,14 @@
-console.log('It Works');
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+            </div>
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById("app"));
